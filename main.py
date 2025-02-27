46b0578a8480150ceebad4dadd1f95de6544910f
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2025-02-28 00:03:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+
+# name:  str=  "GIAIC (Governor Initiative for AI and Computing)"
+# print("GIAIC Stand for :",name)
+
+# #  def  giaic():
+# # print("hello world from Students!")
+
+
+
+# data types
+
+
+# string
+
+text:str = "hello world"
+print(text)
+id_card:str = """
+Name : tabiq
+Father Name : irfan
+age : 24
+"""
+print(id_card)
+
+# integer
+
+
+
+num :int =35
+print(type(num))
+print(num)
+
+
+# float
+
+
+
+float_num :float = [5.7,4 ,3]
+print(type(float_num))
+print(float_num)
+
+
+
+# tuple
+
+
+
+tuple_data : tuple=("apple","cherry","graps")
+print(type(tuple_data))
+print(tuple_data)
+
+
+
+
+# list
+
+
+
+
+
+fruit:list[str]= ["apple","cherry","graps","banana"]
+print(type(fruit))
+print(fruit)
+
+
+
+
+
+
+# boolean
+
+
+
+
+
+
+
+is_true:bool = true 
+is_false:bool = false
+print(type(is_true))
+print(is_true)
+
+
+# dictionary
+
+
+
+person:dict= {"Name:tabiq","age:24","fatherName:irfan"}
+print(type(person))
+print(person)
+
+
+
+
+
+
+# set
+
+
+
+
+unique_numbers:set= {1,2,3,4,5}
+print(type(unique_numbers))
+print(unique_numbers)
